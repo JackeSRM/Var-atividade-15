251023a7dd552fe03801cbe56c35d65f2203fcc7
JackeSRM <127909690+JackeSRM@users.noreply.github.com>
2023-07-04 21:58:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var a = 6;
+console.log(a);
+
+var b = 6;
+console.log(b);
+
+var soma = a + b;
+console.log("Soma:", soma);
+
+var divisao = a / b;
+console.log("Divisão:", divisao);
+
+var multiplicacao = a * b;
+console.log("Multiplicação:", multiplicacao);
+
+var igual = (a === b);
+console.log("Valores são iguais:", igual);
+
+var diferente = (a !== b);
+console.log("Valores são diferentes:", diferente);
+
+var maior = (a > b);
+console.log("a é maior que b:", maior);
+
+var menor = (a < b);
+console.log("a é menor que b:", menor);
